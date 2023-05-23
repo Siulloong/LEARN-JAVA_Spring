355b0097d68930b09ccc0b6a208b30cf55a8966c
Ederson Araujo <siulloong@gmail.com>
2023-05-23 19:40:34 -0300

    Reinicio do zero

--- outros/demo/src/main/java/com/www/demo/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.www.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.www.demo.entities.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
